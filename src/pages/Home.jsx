ea78ee2dd9bb6427e7bc253845301dce3fbf0e09
alvaro <alvaronieves04@gmail.com>
2023-05-09 19:19:50 -0400

    proyecto terminado

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import ListPosts from "../componets/ListPosts";
+import ListCategories from "../componets/ListCategories";
+
+const Home = () => {
+  return (
+    <main>
+      <div className="container">
+        <h2 className="title-page">Pet noticias</h2>
+      </div>
+      <ListCategories />
+      <ListPosts url={"/posts"} />
+    </main>
+  );
+};
+
+export default Home;
